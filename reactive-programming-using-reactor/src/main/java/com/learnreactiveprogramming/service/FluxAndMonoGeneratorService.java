715ed18a3885ff442da1bbc9fa06a00f824b0b57
kcmodev <shchristensen3@gmail.com>
2026-05-26 11:22:16 -0500

    working with flux object

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,17 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux().subscribe(System.out::println);
+    }
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("chris", "steve", "mark", "bill"));
+    }
 }
