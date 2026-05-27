05eea137e1547a1d417c6972390d1a70012a532d
kcmodev <shchristensen3@gmail.com>
2026-05-27 15:31:20 -0500

    updated test name and remove unused import

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -3,14 +3,12 @@ package com.learnreactiveprogramming.service;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FluxAndMonoGeneratorServiceTest {
 
     FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
     @Test
-    void namesFlux() {
+    void namesFluxTest() {
         // when
         var namesFlux = fluxAndMonoGeneratorService.namesFlux();
 
