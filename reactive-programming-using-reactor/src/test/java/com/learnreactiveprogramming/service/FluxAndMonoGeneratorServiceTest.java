ec4361acc89f74e9ef29ebeefa6c9703ced78148
kcmodev <58379109+kcmodev@users.noreply.github.com>
2026-06-10 13:53:02 -0500

    added unit tests for namesFlux and nameMono methods

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,28 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    @Test
+    void mainTest() {
+        FluxAndMonoGeneratorService.main(null);
+    }
+
+    @Test
+    void namesFluxTest() {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        StepVerifier.create(fluxAndMonoGeneratorService.namesFlux())
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMonoTest() {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        StepVerifier.create(fluxAndMonoGeneratorService.nameMono())
+                .expectNext("alex")
+                .verifyComplete();
+    }
 }
